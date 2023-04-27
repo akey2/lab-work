8441808b861f83f001ff0fce4771c5b44d936b8d
zirwin3 <irwinz@uab.edu>
2023-04-27 10:16:08 -0500

    Fixing rig bugs

--- Rig/visPC/ReachTask_SensoryConfound/HomeTarget.gd
+++ Rig/visPC/ReachTask_SensoryConfound/HomeTarget.gd
@@ -1,10 +1,23 @@
 extends Node2D
 
 var color = Color.RED
+var filled = true
+var size = 50
+var size_scaled = 50
 
 func set_color(r, g, b):
 	color = Color8(r, g, b)
 	self.queue_redraw()
+	
+func set_filled(fill):
+	filled = fill
+	self.queue_redraw()
+	
+func set_scaling(scaleperc):
+	size_scaled = (scaleperc/100)*size
+	self.queue_redraw()
+	
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
